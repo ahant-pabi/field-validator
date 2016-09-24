b6a180d54ffa6a2dd7efde2703d802613175b5e6
Pabitra Muni <mailtopabi@gmail.com>
2016-09-24 12:15:41 +0530

    code cleanup

--- src/main/kotlin/com/github/ahant/validator/validation/FieldValidator.kt
+++ src/main/kotlin/com/github/ahant/validator/validation/FieldValidator.kt
@@ -5,7 +5,6 @@ import com.github.ahant.validator.validation.util.CountryCode
 /**
  * Created by ahant on 8/14/2016.
  */
-@FunctionalInterface
 internal interface FieldValidator<in T> {
     fun validate(input: T): Set<String>
     fun setCountry(countryCode: CountryCode){}
